7e49e1f1d765fce16bbf2ca0de1ef2f1936dbc17
tusharbalar <balar.tushar1@gmail.com>
2019-06-24 01:12:00 +0530

    Provider Consumer concept

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
 
 export default class Product extends Component {
-
     render() {
+        const {id, title, img, price, inCart} = this.props.product;
         return(
-            <div>
-                <h3>Hello from product</h3>
-            </div>
+
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5">
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top" />
+                        </Link>
+                        <button className="cart-btn" disabled={inCart?true:false}>
+                            {inCart?(
+                                <p className="text-capitalize mb-0" disabled>{ " " } in inCart</p>
+                            ): (
+                                <i className="fa fa-cart-plus"></i>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper = styled.div``;
