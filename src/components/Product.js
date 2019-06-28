848dd67dab931a33665a3cd5928f753da7f1a115
tusharbalar <balar.tushar1@gmail.com>
2019-06-29 02:02:22 +0530

    Design change

--- src/components/Product.js
+++ src/components/Product.js
@@ -38,4 +38,56 @@ export default class Product extends Component {
     }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+    .card {
+        border-color: transparent;
+        transitions: all 1s linear;
+    }
+    
+    .card-footer {
+        background: transparent;
+        border-top: transparent;
+        transition: all 0.5s linear;
+    }
+    &:hover {
+        .card {
+            border: none;
+            box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2);
+            border-radius: 0px;
+        }
+        .card-footer {
+            border-top: 1px solid lightblue;
+            background: #fff;
+        }
+    }
+    .img-container {
+        position: relative;
+        overflow: hidden;
+    }
+    .card-img-top {
+        transition: all 1s linear;
+    }
+    .img-container:hover .card-img-top {
+        transform: scale(1.2);
+    }
+    .cart-btn {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 0.2rem 0.4rem;
+        background: var(--lightBlue);
+        border: none;
+        color: var(--mainWhite);
+        font-size: 1.4rem;
+        border-radius: 0.5rem 0 0 0;
+        transform: translate(100%, 100%);
+        transition: all 0.3s linear;
+    }
+    .img-container:hover .cart-btn {
+        transform: translate(0, 0);
+    }
+    .cart-btn:hover {
+        color: var(--mainBlue);
+        cursor: pointer;
+    }
+`;
